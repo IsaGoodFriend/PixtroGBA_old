a27efc4ead69178815ec13733888f6c9718ab4ec
Isabel S <goodeveningisabel@gmail.com>
2021-02-22 02:46:49 -0700

    Uploading test code

--- Engine/source/engine.c
+++ Engine/source/engine.c
@@ -3,8 +3,73 @@
 
 #include "pixtro_basic.h"
 
+#include "levels.h"
+#include "load_data.h"
+
+void update();
+void render();
+
+extern unsigned int tileTypes[128];
+
+void test_update(int index) {
+	Entity *ent = &entities[index];
+	ent->velY += 0x20;
+	ent->velX = FIXED_APPROACH(ent->velX, (key_tri_horz() * 0x200), 0x40);
+	
+	
+	if (key_pressed(KEY_A, 20)){
+		ent->velY = -0x280;
+		clear_buffer(KEY_A);
+	}
+	
+	entity_physics(ent, 0x1, 0x1);
+	
+}
+void test_render(int index) {
+	Entity ent = entities[index];
+	
+	draw(ent.x - 0x200, ent.y, 0, 0, 0, 0);
+}
+
+void on_update() {
+	cam_x = FIXED2INT(entities[0].x);
+	cam_y = FIXED2INT(entities[0].y);
+}
+
 void init() {
 	
+	tileTypes[0] = 0x10;
+	
+	entity_update[0] = &test_update;
+	entity_render[0] = &test_render;
+	
+	LOAD_TILESET(test);
+	
+	load_bg_pal(PAL_test, 0);
+	load_obj_pal(PAL_test, 0);
+	
+	load_sprite(&SPR_test_anim[0], 0, SPRITE16x16);
+	
+	set_layer_priority(1, 1);
+	set_layer_priority(2, 2);
+	set_layer_priority(3, 3);
+	
+	LOAD_BG(sample_ase, 1);
+	
+	set_foreground_count(1);
+	finalize_layers();
+	
+	load_collision((unsigned char*)LVL_test);
+	load_midground(0);
+	load_entities();
+	
+	reset_cam();
+	
+	custom_update = &on_update;
+	
+	entities[0].ID |= ENT_ACTIVE_FLAG;
+	entities[0].width = 12;
+	entities[0].height = 16;
 }
 
 void init_settings() {
